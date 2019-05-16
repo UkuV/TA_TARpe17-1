0f32fb709b53ee89234c33e38ec3b91b17a7d538
UkuV <uku.vesper@gmail.com>
2019-05-16 16:17:26 +0300

    Hea ja Paha kangelase klassi tegemine ja laiendamine Kangelane klassiga

---
+++ Kangelased/Kangelased/HeaKangelane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class HeaKangelane : Kangelane
+    {
+        public HeaKangelane(string Nimi, string Asukoht) : base(Nimi, Asukoht)
+        {
+        }
+
+        public HeaKangelane(string Nimi, string SuperNimi, string Võime) : base(Nimi, SuperNimi, Võime)
+        {
+        }
+
+        public override void KasutaVõimet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void VõiduKõne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kangelased/Kangelased/PahaKangelane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class PahaKangelane : Kangelane
+    {
+        public PahaKangelane(string Nimi, string Asukoht) : base(Nimi, Asukoht)
+        {
+        }
+
+        public PahaKangelane(string Nimi, string SuperNimi, string Võime) : base(Nimi, SuperNimi, Võime)
+        {
+        }
+
+        public override void KasutaVõimet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void VõiduKõne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
